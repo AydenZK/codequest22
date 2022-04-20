5c7c404b5967ac4ce3c2acb7ab25cb480264d35b
Ayden Khalil <aydenkhalil619@gmail.com>
2022-04-20 22:19:12 +1000

    changes

--- fighter_bot/main.py
+++ fighter_bot/main.py
@@ -12,29 +12,35 @@ def read_index(player_index, n_players):
     global my_index
     my_index = player_index
 
-my_energy = stats.general.STARTING_ENERGY
-map_data = {}
-spawns = [None]*4
-food = []
-distance = {}
-closest_site = None
-total_ants = 0
+MY_ENERGY = stats.general.STARTING_ENERGY
+MAP_DATA = {}
+SPAWNS = [None]*4
+FOOD = []
+HILL = []
+DISTANCE = {}
+TOTAL_ANTS = 0
+SETTLER_COLOUR = "#41BEB6"
+FIGHTER_COLOUR = "#DE21DB"
+
+ES_W_TO_F: 5 # Energy Strat: worker to fighter ratio
 
 def read_map(md, energy_info):
-    global map_data, spawns, food, distance, closest_site
-    map_data = md
-    for y in range(len(map_data)):
-        for x in range(len(map_data[0])):
-            if map_data[y][x] == "F":
-                food.append((x, y))
-            if map_data[y][x] in "RBYG":
-                spawns["RBYG".index(map_data[y][x])] = (x, y)
+    global MAP_DATA, SPAWNS, FOOD, DISTANCE, CLOSEST_SITE, HILL
+    MAP_DATA = md
+    for y in range(len(MAP_DATA)):
+        for x in range(len(MAP_DATA[0])):
+            if MAP_DATA[y][x] == "F":
+                FOOD.append((x, y))
+            elif MAP_DATA[y][x] == "Z":
+                HILL.append((x, y))
+            if MAP_DATA[y][x] in "RBYG":
+                SPAWNS["RBYG".index(MAP_DATA[y][x])] = (x, y)
     # Read map is called after read_index
-    startpoint = spawns[my_index]
-    # Dijkstra's Algorithm: Find the shortest path from your spawn to each food zone.
+    startpoint = SPAWNS[my_index]
+    # Dijkstra's Algorithm: Find the shortest path from your spawn to each FOOD zone.
     # Step 1: Generate edges - for this we will just use orthogonally connected cells.
     adj = {}
-    h, w = len(map_data), len(map_data[0])
+    h, w = len(MAP_DATA), len(MAP_DATA[0])
     # A list of all points in the grid
     points = []
     # Mapping every point to a number
@@ -43,13 +49,13 @@ def read_map(md, energy_info):
     for y in range(h):
         for x in range(w):
             adj[(x, y)] = []
-            if map_data[y][x] == "W": continue
+            if MAP_DATA[y][x] == "W": continue
             points.append((x, y))
             idx[(x, y)] = counter
             counter += 1
     for x, y in points:
         for a, b in [(y+1, x), (y-1, x), (y, x+1), (y, x-1)]:
-            if 0 <= a < h and 0 <= b < w and map_data[a][b] != "W":
+            if 0 <= a < h and 0 <= b < w and MAP_DATA[a][b] != "W":
                 adj[(x, y)].append((b, a, 1))
     # Step 2: Run Dijkstra's
     import heapq
@@ -57,14 +63,14 @@ def read_map(md, energy_info):
     expanded = [False] * len(points)
     # What nodes are we currently looking at?
     queue = []
-    # What is the distance to the startpoint from every other point?
+    # What is the DISTANCE to the startpoint from every other point?
     heapq.heappush(queue, (0, startpoint))
     while queue:
         d, (a, b) = heapq.heappop(queue)
         if expanded[idx[(a, b)]]: continue
-        # If we haven't already looked at this point, put it in expanded and update the distance.
+        # If we haven't already looked at this point, put it in expanded and update the DISTANCE.
         expanded[idx[(a, b)]] = True
-        distance[(a, b)] = d
+        DISTANCE[(a, b)] = d
         # Look at all neighbours
         for j, k, d2 in adj[(a, b)]:
             if not expanded[idx[(j, k)]]:
@@ -72,57 +78,59 @@ def read_map(md, energy_info):
                     d + d2,
                     (j, k)
                 ))
-    # Now I can calculate the closest food site.
-    food_sites = list(sorted(food, key=lambda prod: distance[prod]))
-    closest_site = food_sites[0]
+    # Now I can calculate the closest FOOD site.
+    FOOD = list(sorted(FOOD, key=lambda prod: DISTANCE[prod]))
+    HILL = list(sorted(HILL, key=lambda prod: DISTANCE[prod]))
 
 def handle_failed_requests(requests):
-    global my_energy
+    global MY_ENERGY
     for req in requests:
         if req.player_index == my_index:
             print(f"Request {req.__class__.__name__} failed. Reason: {req.reason}.")
             raise ValueError()
 
 def handle_events(events):
-    global my_energy, total_ants
+    global MY_ENERGY, TOTAL_ANTS
     requests = []
 
     for ev in events:
         if isinstance(ev, DepositEvent):
             if ev.player_index == my_index:
-                # One of my worker ants just made it back to the Queen! Let's send them back to the food site.
-                requests.append(GoalRequest(ev.ant_id, closest_site))
+                # One of my worker ants just made it back to the Queen! Let's send them back to the FOOD site.
+                requests.append(GoalRequest(ev.ant_id, CLOSEST_SITE))
                 # Additionally, let's update how much energy I've got.
-                my_energy = ev.cur_energy
+                MY_ENERGY = ev.cur_energy
         elif isinstance(ev, ProductionEvent):
             if ev.player_index == my_index:
-                # One of my worker ants just made it to the food site! Let's send them back to the Queen.
-                requests.append(GoalRequest(ev.ant_id, spawns[my_index]))
+                # One of my worker ants just made it to the FOOD site! Let's send them back to the Queen.
+                requests.append(GoalRequest(ev.ant_id, SPAWNS[my_index]))
         elif isinstance(ev, DieEvent):
             if ev.player_index == my_index:
                 # One of my workers just died :(
-                total_ants -= 1
+                TOTAL_ANTS -= 1
 
     # Can I spawn ants?
     spawned_this_tick = 0
+    
+    
     while (
-        total_ants < stats.general.MAX_ANTS_PER_PLAYER and 
+        TOTAL_ANTS < stats.general.MAX_ANTS_PER_PLAYER and 
         spawned_this_tick < stats.general.MAX_SPAWNS_PER_TICK and
-        my_energy >= stats.ants.Worker.COST
+        MY_ENERGY >= stats.ants.Worker.COST
     ):
-        if my_energy < 700:
+        if MY_ENERGY < 700:
             spawned_this_tick += 1
-            total_ants += 1
+            TOTAL_ANTS += 1
             # Spawn an ant, give it some id, no color, and send it to the closest site.
             # I will pay the base cost for this ant, so cost=None.
-            requests.append(SpawnRequest(AntTypes.WORKER, id=None, color=None, goal=closest_site))
+            requests.append(SpawnRequest(AntTypes.WORKER, id=None, color=None, goal=CLOSEST_SITE))
             my_energy -= stats.ants.Worker.COST
         else:
             spawned_this_tick += 1
-            total_ants += 1
+            TOTAL_ANTS += 1
             # Spawn an ant, give it some id, no color, and send it to the closest site.
             # I will pay the base cost for this ant, so cost=None.
-            requests.append(SpawnRequest(AntTypes.SETTLER, id=None, color=None, goal=closest_site))
+            requests.append(SpawnRequest(AntTypes.SETTLER, id=None, color=None, goal=CLOSEST_SITE))
             my_energy -= stats.ants.Worker.COST
 
 
